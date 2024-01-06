b7ba06d1d4368c6d0445452144daab3b754121f2
Tatsuhiko Seki <osekiharn@gmail.com>
2024-01-06 19:21:28 +0900

    不要なファイル削除

--- index.ts
+++ index.ts
@@ -6,7 +6,6 @@ const srcDir = resolve(import.meta.dir, "src");
 const publicDir = resolve(import.meta.dir, "public");
 const distDir = resolve(import.meta.dir, "dist");
 const popup = Bun.file(join(publicDir, "popup.html"));
-const panel = Bun.file(join(publicDir, "panel.html"));
 const devtools = Bun.file(join(publicDir, "devtools.html"));
 const manifest = Bun.file(join(publicDir, "manifest.json"));
 const css = Bun.file(join(publicDir, "popup.css"));
@@ -31,9 +30,7 @@ watch(
   async (event, filename) => {
     await Bun.build({
       entrypoints: [
-        join(srcDir, "background.ts"),
         join(srcDir, "contentScript.ts"),
-        join(srcDir, "panel.ts"),
         join(srcDir, "devtools.ts"),
         join(srcDir, "index.jsx"),
       ],
@@ -47,7 +44,6 @@ watch(
     });
     await Bun.write(join(distDir, "popup.html"), popup);
     await Bun.write(join(distDir, "devtools.html"), devtools);
-    await Bun.write(join(distDir, "panel.html"), panel);
     await Bun.write(join(distDir, "manifest.json"), manifest);
     await Bun.write(join(distDir, "popup.css"), css);
   }
--- src/background.ts
+++
@@ -1,9 +0,0 @@
-import { ACTION } from "./constants.ts";
-
-chrome.runtime.onMessage.addListener((message) => {
-  if (message.action === ACTION.REQUEST_COMPLETED_FROM_DEVTOOLS) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id!, message);
-    });
-  }
-});
